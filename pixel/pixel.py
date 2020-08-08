c4c3d3c61167020f3f4211806335e9ceba7647af
GitAcrown <Acrown@outlook.fr>
2020-08-08 13:37:45 +0200

    Test

--- pixel/pixel.py
+++ pixel/pixel.py
@@ -398,11 +398,14 @@ class Pixel(commands.Cog):
                 em.add_field(name="Navigation", value=options_txt, inline=False)
                 em.set_footer(text="Cliquez sur la réaction correspondante à l'action voulue")
                 msg = await ctx.send(embed=em)
+                def pred(r, u):
+                    logger.info("Reaction = {}, User= {}").format(repr(r), repr(u))
+                    return str(u) == str(author) and r.message.id == msg.id
                 start_adding_reactions(msg, emojis)
                 try:
                     logger.info("En attente de réaction...")
                     react, user = await self.bot.wait_for("reaction_add",
-                                                          check=lambda r, u: str(u) == str(author) and r.message.id == msg.id,
+                                                          check=pred,
                                                           timeout=30)
                 except asyncio.TimeoutError:
                     await msg.delete()
